aa7326f40b866ca877ef2c576eb60e857ef4a4c9
Srishti <srishtiarora1515@gmail.com>
2020-05-25 13:58:41 +0530

    first commit

---
+++ connections.js
@@ -0,0 +1,12 @@
+var followers=[]
+var following=[]
+var requests=[]
+// follow requests will have 2 options that is 
+//either connect or decline. when the connect button 
+//is clicked then the user is entered in the array 
+// of connections.
+// var input - temporary input
+//var user - user whose request has arrived
+if(input==="yes"){
+	
+}
---
+++ signup.js
@@ -0,0 +1,12 @@
+function signupclicked(){
+	if (username==="srishti"){
+		if (password==="srishtiarora"){
+			if (2password==="srishtiarora"){
+				first_page="file:///C:/Users/Srishti/Documents/semester%204/html_css/EAD%20project/first_page.html" 
+			}
+			else{
+				msg="password does not match"
+			}
+		}
+	}
+}
